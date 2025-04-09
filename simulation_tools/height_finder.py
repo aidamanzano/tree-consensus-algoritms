c3242cb14415dcbafcdb4ca7696f0ff038166e97
A. M Manzano Kharman <aidammk@hotmail.com>
2025-04-09 16:16:21 +0100

    height finder correction

--- simulation_tools/height_finder.py
+++ simulation_tools/height_finder.py
@@ -1,14 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-d = np.arange(0, 10, 1)
+# Define ranges
+h = np.arange(0, 10, 1)
 rho = np.arange(0, 1, 0.1)
-y = rho * np.exp(d)
-print(len(rho))
-print(len(d))
-print(len(y))
-plt.plot(d, y, label='rho=0')
-plt.title(r'Height vs. $\rho^d$')
-plt.xlabel(r'Height ($h$)')
-plt.ylabel(r'$\rho ^d$')
+
+# Create meshgrid
+RHO, H = np.meshgrid(rho, h)
+
+# Compute rho^h
+
+Z = RHO ** H
+
+# Create 3D plot
+fig = plt.figure(figsize=(10, 7))
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot the surface
+surf = ax.plot_surface(RHO, H, Z, cmap='viridis', edgecolor='none')
+
+# Add labels and title
+ax.set_xlabel(r'$\rho$')
+ax.set_ylabel(r'Height ($h$)')
+ax.set_zlabel(r'$\rho^h$')
+ax.set_title(r'3D Surface Plot of $\rho^h$')
+
+# Add color bar
+fig.colorbar(surf, ax=ax, shrink=0.5, aspect=10)
+
+plt.tight_layout()
 plt.show()
+
